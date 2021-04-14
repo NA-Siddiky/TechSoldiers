51c1ba6ff7ec2781e0a11e9ed6a3186d941c74f2
NA-Siddiky <siddiky.academic@gmail.com>
2021-04-15 02:57:38 +0600

    addind footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home/Home';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Footer from '../../Shared/Footer/Footer';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Homepage</h1>
+            <Header></Header>
+            <Footer></Footer>
         </div>
     );
 };
