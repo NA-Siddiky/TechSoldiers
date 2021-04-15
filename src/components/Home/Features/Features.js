af25a7ab81b4b5e7b79ac92d9edeb3a72c4ff9b3
NA-Siddiky <siddiky.academic@gmail.com>
2021-04-16 01:16:30 +0600

    font page design

--- src/components/Home/Features/Features.js
+++ src/components/Home/Features/Features.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import './Features.css'
+import images from '../../../images/Banner.jpg'
+import images2 from '../../../images/image2.png'
 
 const Features = () => {
     return (
-        <div>
-            <h2>This is static page for special Features page</h2>
+        <div class="main-features">
+            <div className="blog-card">
+                <input type="radio" name="select" id="tap-1" checked />
+                <input type="radio" name="select" id="tap-2" />
+                <input type="radio" name="select" id="tap-3" />
+                <input type="checkbox" id="imgTap" />
+                <div class="sliders">
+                    <label for="tap-1" class="tap tap-1"></label>
+                    <label for="tap-2" class="tap tap-2"></label>
+                </div>
+                <div class="inner-part">
+                    <label for="imgTap" class="img">
+                        <img src={images} alt="" />
+                    </label>
+                    <div class="content content-1">
+                        <span>26 December 2017</span>
+                        <div class="title">
+                            Lorem Ipsum Dolor</div>
+                        <div class="text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo animi atque aliquid pariatur voluptatem numquam, quisquam. Neque est voluptates doloribus!</div>
+                        <button>Read more</button>
+                    </div>
+                </div>
+                <div class="inner-part">
+                    <label for="imgTap" class="img">
+                        <img src={images2} alt="" />
+                    </label>
+                    <div class="content content-2">
+                        <span>26 December 2018</span>
+                        <div class="title">
+                            Lorem Ipsum Dolor</div>
+                        <div class="text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum eos ut consectetur numquam ullam fuga animi laudantium nobis rem molestias.</div>
+                        <button>Read more</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import './Services.css'
 
 const Services = () => {
+
+
     return (
-        <div>
-            <h1>This is services</h1>
+        <div class="main-services">
+            <div class="container">
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <img src="../../../images/Banner.jpg" alt="" />
+                            <h3>Web Design</h3>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <img src="../../../images/Banner.jpg" alt="" />
+                            <h3>Domain & Hosting</h3>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <img src="../../../images/Banner.jpg" alt="" />
+                            <h3>IT Enable Services</h3>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
