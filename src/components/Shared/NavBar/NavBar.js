7d0524541305c05caef0aaf077af1e2d64fac58b
NA-Siddiky <siddiky.academic@gmail.com>
2021-04-16 03:35:56 +0600

    adding navber

--- src/components/Shared/NavBar/NavBar.js
+++ src/components/Shared/NavBar/NavBar.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import { Context } from '../../Login/Context/Context';
+import { RiLogoutCircleRLine } from 'react-icons/ri';
 
 const NavBar = () => {
 
@@ -32,54 +33,54 @@ const NavBar = () => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid">
-                <Link className="navbar-brand brandName" to="/">
-                    <h4>Tech-Soldiers</h4>
-                </Link>
-                <div
-                    className=" navbar-collapse justify-content-end"
-                    id="navbarSupportedContent"
-                >
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">
-                                Home
-							</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/orders">
-                                Orders
-							</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/admin/addproduct">
-                                Admin
-							</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/deals">
-                                Deals
-							</Link>
-                        </li>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-                        {isSignedIn ? (
-                            <>
-                                <li className="nav-item">
-                                    <p>{name}</p>
-                                </li>
-                                <li className="nav-item">
-                                    <button className="btn btn-primary" onClick={handleSignOut}>
-                                        Logout
-									</button>
-                                </li>
-                            </>
-                        ) : (
-                            <Link className="nav-link" to="/login">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <Link className="nav-link mr-4" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item active">
+                        <Link className="nav-link mr-5" to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li className="nav-item active">
+                        <Link className="nav-link mr-4" to="/dashboard">Admin</Link>
+                    </li>
+                    <li className="nav-item active">
+                        <Link className="nav-link mr-4" to="#">Blogs</Link>
+                    </li>
+                    <li className="nav-item active">
+                        <Link className="nav-link mr-4" to="#">Contact Us</Link>
+                    </li>
+
+                    {!isSignedIn ? (
+                        <li className="nav-item">
+                            <Link className="nav-link  btn btn-primary" to="/login">
                                 Login
                             </Link>
-                        )}
-                    </ul>
-                </div>
+                        </li>
+                    ) : (
+                        <li className="nav-item d-flex">
+                            <button
+                                className="btn btn-success "
+                                onClick={handleSignOut}
+                            >
+                                {name}
+                                <RiLogoutCircleRLine />
+                            </button>
+                        </li>
+                    )}
+                </ul>
             </div>
         </nav>
     );
